7521f6ac5448d237b801e3f98d666493dce0cc5e
Hassan Khan <hf610061@gmail.com>
2022-07-10 04:30:22 +0500

    new

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+// start tab 
 function tics(arg){
     var btn0 = document.getElementById("btn0");
     var tab0 = document.getElementById("tab0");
@@ -8,3 +9,19 @@ function tics(arg){
         
     }
 }
+// start tab end
+
+//questions
+var nextbtn0 = document.getElementById("nextbtn0"); 
+var nexttab0 = document.getElementById("nexttab0");
+function next(arg){
+    if(arg === "0"){
+        tab0.style.display = "none"
+        nexttab0.style.display = "block";
+        nextbtn0.style.display = "none"
+    }else{
+
+        nexttab0.style.display = "block";
+    }
+} 
+    
