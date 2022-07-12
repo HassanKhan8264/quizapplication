48cf42ce531d36999f8a68d7166e4a81e111d0da
Hassan Khan <hf610061@gmail.com>
2022-07-12 23:40:00 +0500

    new

--- start.js
+++ start.js
@@ -1,27 +1,29 @@
 // start tab 
-function tics(arg){
+function tics(arg) {
     var btn0 = document.getElementById("btn0");
     var tab0 = document.getElementById("tab0");
-    if(arg === "0"){
+    if (arg === "zero") {
         tab0.style.display = "block"
-    }else{
+    } else {
         tab0.style.display = "none"
-        
+
     }
 }
 // start tab end
 
 //questions
-var nextbtn0 = document.getElementById("nextbtn0"); 
-var nexttab0 = document.getElementById("nexttab0");
-function next(arg){
-    if(arg === "0"){
-        tab0.style.display = "none"
-        nexttab0.style.display = "block";
-        nextbtn0.style.display = "none"
-    }else{
+function check() {
+    score = 0;
 
-        nexttab0.style.display = "block";
+    var right = document.getElementById("q1");
+    var q2 = document.getElementById("q2")
+    var q3 = document.getElementById("q3")
+    var q4 = document.getElementById("q4")
+    if (right.checked === true) {
+        score++;
+        alert("You are right how do you know")
+    } else {
+        score--;
     }
+
 } 
-    
